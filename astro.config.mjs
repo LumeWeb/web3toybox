8e920e41e01b457fd4e4f9980827c6cb2b792439
Juan Di Toro <ditorojuanf@gmail.com>
2023-10-30 17:53:53 +0100

    fix: improve styles and local https setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,30 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config"
+import * as fs from "node:fs"
 
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import optimizer from 'vite-plugin-optimizer'
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import optimizer from "vite-plugin-optimizer"
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind({ applyBaseStyles: false, })],
-    vite: {
-        build: {
-            minify: false
-        },
-        resolve: { 'dedupe': ['@lumeweb/libportal', '@lumeweb/libweb', '@lumeweb/libkernel'] },
-        plugins: [
-            optimizer({
-                'node-fetch':
-                    'const e = undefined; export default e;export {e as Response, e as FormData, e as Blob};',
-            }),
-        ]
-    }
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  vite: {
+    server: {
+      https: {
+        cert: fs.readFileSync("./.local-ssl/localhost.pem"),
+        key: fs.readFileSync("./.local-ssl/localhost-key.pem")
+      },
+    },
+    build: {
+      minify: false
+    },
+    resolve: {
+      dedupe: ["@lumeweb/libportal", "@lumeweb/libweb", "@lumeweb/libkernel"]
+    },
+    plugins: [
+      optimizer({
+        "node-fetch":
+          "const e = undefined; export default e;export {e as Response, e as FormData, e as Blob};"
+      })
+    ]
+  }
 })
